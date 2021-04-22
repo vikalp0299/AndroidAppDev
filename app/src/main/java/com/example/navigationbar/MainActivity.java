6c58d3de0c6074a9e190c9b5f6923376d5606ded
Vikalp Parashar <vparashar0299@gmail.com>
2021-04-22 09:27:16 +0530

    Navigation Bar Ready

--- app/src/main/java/com/example/navigationbar/MainActivity.java
+++ app/src/main/java/com/example/navigationbar/MainActivity.java
@@ -1,21 +1,69 @@
 package com.example.navigationbar;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.SurfaceControl;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.widget.ViewPager2;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import com.example.navigationbar.fragments.AssignmentFragment;
+import com.example.navigationbar.fragments.CalendarFragment;
+import com.example.navigationbar.fragments.ChatFragment;
+import com.example.navigationbar.fragments.NotificationFragment;
+import com.example.navigationbar.fragments.TeamsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-BottomNavigationView bottomNavigationView;
+    BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.notification:
+                        showFragment(NotificationFragment.newInstance("",""));
+                        return true;
+                    case R.id.Chats:
+                        showFragment(ChatFragment.newInstance("",""));
+                        return true;
+                    case R.id.Teams:
+                        showFragment(TeamsFragment.newInstance("",""));
+                        return true;
+                    case R.id.Assignment:
+                        showFragment(AssignmentFragment.newInstance("",""));
+                        return true;
+                    case R.id.Calender:
+                        showFragment(CalendarFragment.newInstance("",""));
+                        return true;
 
+                    default:
+                        return false;
+                }
+            }
+        });
+        showFragment(NotificationFragment.newInstance("",""));
+    }
+
+    private void makeText(String message) {
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void showFragment(Fragment fragment)
+    {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.Framelayout,fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
+
+
