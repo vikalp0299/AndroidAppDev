c66b8f2aac10f95f4fb6487cd73826f4018d68a1
Vikalp Parashar <vparashar0299@gmail.com>
2021-04-22 08:18:42 +0530

    destroy

--- app/src/main/java/com/example/navigationbar/MainActivity.java
+++ app/src/main/java/com/example/navigationbar/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.navigationbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        bottomNavigationView =(BottomNavigationView)findViewById(R.id.bottom_nav);
+       bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+           @Override
+           public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+               return false;
+           }
+       });
+
     }
 }
