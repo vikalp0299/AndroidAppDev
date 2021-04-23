9ca566565797d053fed1dee44cc8bf832bae1872
freakbeast14 <shahilmohammed14@gmail.com>
2021-04-23 10:55:04 +0530

    added sign up button

--- app/src/main/java/com/example/navigationbar/LoginActivity.java
+++ app/src/main/java/com/example/navigationbar/LoginActivity.java
@@ -15,7 +15,7 @@ import android.widget.Toast;
 public class LoginActivity extends AppCompatActivity {
 
 
-    private EditText Password, LearnerId;
+    private EditText Password, UserName;
     private TextView ForgotPassword;
 
     @Override
@@ -24,10 +24,11 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         // declaring object of EditText control
-        LearnerId = (EditText) findViewById(R.id.LearnerId);
+        UserName = (EditText) findViewById(R.id.UserName);
         Password = (EditText) findViewById(R.id.Password);
         ForgotPassword = (TextView) findViewById(R.id.ForgotPassword);
         Button LoginBtn = (Button) findViewById(R.id.LoginBtn);
+        Button SignUpBtn = (Button) findViewById(R.id.SignUpBtn);
 
         ForgotPassword.setOnClickListener(new OnClickListener() {
             @Override
@@ -37,29 +38,52 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        //On pressing Login button
         LoginBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                String LId = LearnerId.getText().toString();
+                String Usr = UserName.getText().toString();
                 String Pwd = Password.getText().toString();
+                String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
-                if(LId.equalsIgnoreCase("ADMIN") && Pwd.equals("ADMIN")){
-                    Intent MainIntent = new Intent(LoginActivity.this, MainActivity.class);
-                    Toast.makeText(LoginActivity.this,"You are Signed in Successfully", Toast.LENGTH_LONG).show();
-                    startActivity(MainIntent);
-                    finish();
-                }else
-                {
-                    if(LId.isEmpty()) {
-                        Toast.makeText(getApplicationContext(),"enter learner id",Toast.LENGTH_SHORT).show();
-                    }
-                    else if(Pwd.isEmpty()) {
-                        Toast.makeText(getApplicationContext(),"enter password",Toast.LENGTH_SHORT).show();
-                    }
-                    else{
-                    Toast.makeText(LoginActivity.this, "User Name or Password is incorrect", Toast.LENGTH_LONG).show();
+                //checks if username is empty
+                if(Usr.isEmpty()) {
+                    Toast.makeText(getApplicationContext(),"enter username",Toast.LENGTH_SHORT).show();
                 }
+
+                //checks if password is empty
+                else if(Pwd.isEmpty()) {
+                    Toast.makeText(getApplicationContext(),"enter password",Toast.LENGTH_SHORT).show();
                 }
+
+                //checks if username is valid or not
+                else if (!Usr.trim().matches(emailPattern)) {
+                    Toast.makeText(getApplicationContext(),"invalid email address",Toast.LENGTH_SHORT).show();
+                }
+
+                else {
+                    //authentication
+                    if (Usr.equalsIgnoreCase("ADMIN@gmail.com") && Pwd.equals("ADMIN")) {
+                        Intent MainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                        Toast.makeText(LoginActivity.this, "You are Signed in Successfully", Toast.LENGTH_LONG).show();
+                        startActivity(MainIntent);
+                        finish();
+                    }
+
+                    //if authentication failed
+                    else {
+                        Toast.makeText(LoginActivity.this, "User Name or Password is incorrect", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+        //On pressing Sign Up button
+        SignUpBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "Redirecting...", Toast.LENGTH_LONG).show();
+                //TODO: Redirect to web sign up page
             }
         });
 
