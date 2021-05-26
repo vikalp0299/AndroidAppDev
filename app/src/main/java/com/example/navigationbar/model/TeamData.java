298c9e4a6cada0ba4d2d2cd4d50e87e833eb4225
freakbeast14 <shahilmohammed14@gmail.com>
2021-05-27 00:41:10 +0530

    executed sharedpreferences to retain created teams

--- app/src/main/java/com/example/navigationbar/model/TeamData.java
+++ app/src/main/java/com/example/navigationbar/model/TeamData.java
@@ -1,40 +1,40 @@
-package com.example.navigationbar.data;
+package com.example.navigationbar.model;
 
 
 public final class TeamData {
 
-    private String userName;
-    private String userMb;
+    private String teamName;
+    private String teamDetail;
 
 
     public final String getTeamName() {
-        return this.userName;
+        return this.teamName;
     }
 
     public final void setTeamName(String var1) {
-        this.userName = var1;
+        this.teamName = var1;
     }
 
     public final String getTeamDetail() {
-        return this.userMb;
+        return this.teamDetail;
     }
 
     public final void setTeamDetail(String var1) {
-        this.userMb = var1;
+        this.teamDetail = var1;
     }
 
-    public TeamData(String userName, String userMb) {
+    public TeamData(String teamName, String teamDetail) {
         super();
-        this.userName = userName;
-        this.userMb = userMb;
+        this.teamName = teamName;
+        this.teamDetail = teamDetail;
     }
 
     public final String component1() {
-        return this.userName;
+        return this.teamName;
     }
 
     public final String component2() {
-        return this.userMb;
+        return this.teamDetail;
     }
 
     public final TeamData copy(String userName, String userMb) {
@@ -43,24 +43,24 @@ public final class TeamData {
 
     public static TeamData copy$default(TeamData var0, String var1, String var2, int var3, Object var4) {
         if ((var3 & 1) != 0) {
-            var1 = var0.userName;
+            var1 = var0.teamName;
         }
 
         if ((var3 & 2) != 0) {
-            var2 = var0.userMb;
+            var2 = var0.teamDetail;
         }
 
         return var0.copy(var1, var2);
     }
 
     public String toString() {
-        return "TeamData(userName=" + this.userName + ", userMb=" + this.userMb + ")";
+        return "TeamData(teamName=" + this.teamName + ", teamDetail=" + this.teamDetail + ")";
     }
 
     public int hashCode() {
-        String var10000 = this.userName;
+        String var10000 = this.teamName;
         int var1 = (var10000 != null ? var10000.hashCode() : 0) * 31;
-        String var10001 = this.userMb;
+        String var10001 = this.teamDetail;
         return var1 + (var10001 != null ? var10001.hashCode() : 0);
     }
 
